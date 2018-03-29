d859e87cab14872ed7163d83d6148c15310fcfdf
Vasiliev Evgenij <vasiliev.evgenij@gmail.com>
2018-03-29 22:37:50 +0300

    HW04_test_not_working

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,40 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.GETALL, query = "SELECT m FROM Meal m WHERE  m.user.id=:user_id"),
+        @NamedQuery(name = Meal.GETBETWEEN, query = "SELECT m FROM Meal m WHERE  m.user.id=:user_id ORDER BY m.dateTime DESC")
+})
+
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE ="Meal.delete";
+    public static final String GET ="Meal.get";
+    public static final String GETALL ="Meal.getall";
+    public static final String GETBETWEEN ="Meal.getbetween";
+
+    @Column(name = "date_time")
+    @NotBlank
     private LocalDateTime dateTime;
 
+    @Column(name = "description")
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String description;
 
+    @Column(name = "calories")
+    @NotNull
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
